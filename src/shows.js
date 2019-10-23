265db87451ab391ce3d60643b7146dccd6005eec
nik frank <nikbfrank@gmail.com>
2019-10-23 21:07:35 +0300

    starting json for tv

---
+++ src/shows.js
@@ -0,0 +1,197 @@
+export default [
+  {
+    title: 'Better Call Saul',
+
+    cardImage: 'https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1440,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1533095559/180801-wilstein-better-call-saul-hero_zc97f2',
+
+
+    description: `He wasn't always Saul Goodman, ace attorney for chemist-turned-meth dealer Walter White. Six years before he begins to represent Albuquerque's most notorious criminal, Goodman is Jimmy McGill, a small-time attorney hustling to make a name for himself.`,
+
+    episodes: 40,
+    
+    cast: [
+      { actor: 'Bob Odenkirk', character: 'Jimmy McGill', episodes: 40 },
+      { actor: 'Jonathan Banks', character: 'Mike Ehrmantraut', episodes: 40 },
+      { actor: 'Rhea Seehorn', character: 'Kim Wexler', episodes: 40 },
+      { actor: 'Patrick Fabian', character: 'Howard Hamlin', episodes: 40 },
+      { actor: 'Michael Mando', character: 'Nacho Varga', episodes: 40 },
+      { actor: 'Michael McKean', character: 'Chuck McGill', episodes: 32 },
+      { actor: 'Giancarlo Esposito', character: `Gustavo 'Gus' Fring`, episodes: 19 },
+      { actor: 'Mark Margolis', character: 'Hector Salamanca', episodes: 15 },
+      { actor: 'Kerry Condon', character: 'Stacey Ehrmantraut', episdoes: 14 },
+    ],
+  },
+
+  {
+    title: 'Breaking Bad',
+
+    cardImage: 'https://www.tvinsider.com/wp-content/uploads/2019/02/AMC-Breaking_Bad-5_1738-1014x570.jpg',
+
+    description: `Mild-mannered high school chemistry teacher Walter White thinks his life can't get much worse. His salary barely makes ends meet, and is dumbstruck when he learns he has terminal cancer. Realizing that his illness probably will ruin his family financially, Walter makes a desperate bid to earn as much money as he can in the time he has left by turning an old RV into a meth lab on wheels.`,
+
+    episodes: 62,
+
+    cast: [
+      { actor: 'Bryan Cranston', character: 'Walter White', episodes: 62 },
+      { actor: 'Anna Gunn', character: 'Skyler White', episodes: 62 },
+      { actor: 'Aaron Paul', character: 'Jesse Pinkman', episodes: 62 },
+      { actor: 'Dean Norris', character: 'Hank Schrader', episodes: 62 }, 
+      { actor: 'Betsy Brandt', character: 'Marie Schrader', episodes: 62 },
+      { actor: 'RJ Mitte', character: 'Walter White, Jr.', episodes: 62 }, 
+      { actor: 'Bob Odenkirk', character: 'Saul Goodman', episodes: 43 },
+      { actor: 'Steven Michael Quezada', character: 'Steven Gomez', episodes: 34 },
+      { actor: 'Jonathan Banks', character: 'Mike Ehrmantraut', episodes: 28 },
+      { actor: 'Giancarlo Esposito', character: `Gustavo 'Gus' Fring`, episodes: 26 },
+      { actor: 'Charles Baker', character: 'Skinny Pete', episodes: 15 },
+      { actor: 'Jesse Plemons', character: 'Todd', episodes: 13 },
+      { actor: 'Christopher Cousins', character: 'Ted Beneke', episodes: 13 },
+      { actor: 'Laura Fraser', character: 'Lydia Rodarte-Quayle', episodes: 12 },
+      { actor: 'Matt Jones', character: 'Badger', episodes: 12 },
+      { actor: 'Mark Margolis', character: 'Hector Salamanca', episodes: 8 },
+    ],
+  },
+                                    
+  {
+    title: 'Bojack Horseman',
+
+    cardImage: 'https://cdn.vox-cdn.com/thumbor/_U_4VYZN8P5Ce-zNrHMkaQ_tRds=/0x0:2000x1286/1200x675/filters:focal(840x483:1160x803)/cdn.vox-cdn.com/uploads/chorus_image/image/60793407/bojack_horseman_lead.0.jpg',
+
+    description: `A humanoid horse, BoJack Horseman -- lost in a sea of self-loathing and booze -- decides it's time for a comeback. Once the star of a '90s sitcom, 18 years after its sudden cancellation, BoJack wants to regain his dignity. With the aid of a human sidekick and a feline ex-girlfriend who is his agent, he sets out to make it happen. But Hollywood is vastly different from those days, and getting used to stuff like Twitter may take some time.`,
+
+    episodes: 61,
+
+    cast: [
+      { actor: 'Will Arnett', character: 'BoJack Horseman', episodes: 61 },
+      { actor: 'Amy Sedaris', character: 'Princess Carolyn', episodes: 61 },
+      { actor: 'Alison Brie', character: 'Diane Nguyen', episodes: 61 },
+      { actor: 'Aaron Paul', character: 'Todd Chavez', episodes: 61 },
+      { actor: 'Paul F. Tompkins', character: 'Mr. Peanutbutter', episodes: 59 },
+      { actor: 'Adam Conover', character: 'A Ryan Seacrest Type', episodes: 17 },
+      { actor: 'Keith Olbermann', character: 'Tom Jumbo-Grumbo', episodes: 15 },
+      { actor: 'Raphael Bob-Waksberg', character: 'Charley Witherspoon' , episodes: 11 },
+      { actor: 'Kristen Schaal', character: 'Sarah Lynn' , episodes: 11 },
+      { actor: 'Patton Oswalt', character: 'Pinky Penguin' , episodes: 11 },
+      { actor: 'Angela Bassett', character: 'Ana Spanikopita', episodes: 10 },
+      { actor: 'Rami Malek', character: 'Flip McVicker', episodes: 10 },
+      { actor: 'Wendie Malick', character: 'Beatrice Horseman', episodes: 10 },
+      { actor: 'J.K. Simmons', character: 'Lenny Turtletaub', episodes: 10 },
+    ],
+  },
+
+
+  {
+    title: 'Arrested Development',
+
+    cardImage: 'https://vignette.wikia.nocookie.net/arresteddevelopment/images/d/d5/Season_2_-_Character_Promo_01.jpeg/revision/latest?cb=20130116183441',
+
+
+    description: 'Michael Bluth finds himself forced to stay in Orange County and run the family real estate business after his father, George Bluth Sr., is sent to prison for committing white-collar crime. He tries to juggle the wants and needs of his spoiled and eccentric family while being a good role model for his teenage son, George Michael.',
+
+    episodes: 84,
+
+    cast: [
+      { actor: 'Jason Bateman', character: 'Michael Bluth', episodes: 84 },
+      { actor: 'Portia de Rossi', character: 'Lindsay Bluth Fünke', episodes: 84 },
+      { actor: 'Will Arnett', character: 'Gob Bluth', episodes: 84 },
+      { actor: 'Michael Cera', character: 'George-Michael Bluth', episodes: 84 },
+      { actor: 'Alia Shawkat', character: 'Maeby Fünke', episodes: 84 },
+      { actor: 'Tony Hale ', character: 'Buster Bluth', episodes: 84 },
+      { actor: 'David Cross', character: 'Tobias Fünke', episodes: 84 },
+      { actor: 'Jeffrey Tambor', character: 'George Bluth Sr.', episodes: 84 },
+      { actor: 'Jessica Walter', character: 'Lucille Bluth', episodes: 84 },
+      { actor: 'Ron Howard', character: 'Narrator', episodes: 84 },
+      { actor: 'Henry Winkler', character: 'Barry Zuckerkorn', episodes: 32 },
+      { actor: 'John Beard', character: 'John Beard', episodes: 31 },
+      { actor: 'Liza Minnelli', character: 'Lucille Austero', episodes: 21 },
+      { actor: 'Isla Fisher', character: 'Rebel Alley', episodes: 16 },
+      { actor: 'Mae Whitman', character: 'Ann Veal', episodes: 16 },
+      { actor: 'Justin Grant Wade', character: 'Steve Holt', episodes: 16 },
+      { actor: 'Ed Begley Jr.', character: 'Stan Sitwell', episodes: 15 },
+      { actor: 'Maria Bamford', character: 'Debrie Bardeaux', episodes: 15 },
+      { actor: 'Kyle Mooney', character: 'Murphy Brown', episodes: 14 },
+      { actor: 'Judy Greer', character: 'Kitty Sanchez', episodes: 13 },
+      { actor: 'B.W. Gonzalez', character: 'Lupe', episodes: 12 },
+      { actor: 'Justin Lee', character: 'Annyong Bluth', episodes: 12 },
+      { actor: 'Christine Taylor', character: 'Sally Sitwell', episodes: 11 },
+      { actor: 'Jeff Garlin', character: 'Mort Meyers', episodes: 11 },
+    ],
+  },
+
+  {
+    title 'Curb Your Enthusiasm',
+
+    cardImage: 'https://ksassets.timeincuk.net/wp/uploads/sites/55/2016/06/2014CurbYourEnthusiasm_080413.jpg',
+
+    description: `'Seinfeld' co-creator Larry David plays a version of himself on the improvised series. He faces a constant barrage of life's little annoyances, which in David's sometimes well-meaning but terminally fumbling hands don't tend to stay small for very long.`,
+
+    episodes: 90,
+
+    cast: [
+      { actor: 'Larry David', character: 'Larry David', episodes: 93 },
+      { actor: 'Jeff Garlin', character: 'Jeff Greene', episodes: 92 },
+      { actor: 'Cheryl Hines', character: 'Cheryl David', episodes: 83 },
+      { actor: 'Susie Essman', character: 'Susie Greene', episodes: 64 },
+      { actor: 'Richard Lewis', character: 'Richard Lewis', episodes: 34 },
+      { actor: 'J.B. Smoove', character: 'Leon Black', episodes: 29 },
+      { actor: 'Bob Einstein', character: 'Marty Funkhouser', episodes: 22 },
+      { actor: 'Ted Danson', character: 'Ted Danson', episodes: 21 },
+      { actor: 'Ashly Holloway', character: 'Sammi Greene', episodes: 17 },
+      { actor: 'Shelley Berman', character: 'Nat David', episodes: 13 },
+      { actor: 'Antoinette Spolar', character: 'Antoinette', episodes: 13 },
+      { actor: 'Vivica A. Fox', character: 'Loretta Black', episodes: 11 },
+      { actor: 'Carla Jeffery', character: 'Keysha Black', episodes: 10 },
+      { actor: 'Nick Nervies', character: 'Daryl Black', episodes: 10 },
+      { actor: 'Ann Ryerson', character: 'Nan Funkhouser', episodes: 10 },
+      { actor: 'Wanda Sykes', character: 'Wanda Sykes', episodes: 9 },
+      { actor: 'Ellia English', character: 'Auntie Rae', episodes: 9 },
+      { actor: 'Saverio Guerra', character: 'Mocha Joe', episodes: 9 },
+      { actor: 'Julia Louis-Dreyfus', character: 'Julia Louis-Dreyfus', episodes: 9 },
+    ],
+  },
+
+  {
+    title: 'Mr Show with Bob and David',
+
+    cardImage: 'https://m.media-amazon.com/images/M/MV5BYTMwN2ZhYjYtYjZkZi00MWYwLTg3ZWYtZDQ3MjczNTExODRlXkEyXkFqcGdeQXRoaXJkLXBhcnR5LXZpZGVvLXVwZGF0ZXI@._V1_CR0,45,480,270_AL_UX477_CR0,0,477,268_AL_.jpg',
+
+    description: 'This is a sketch-comedy show. The twist here is that all of the sketches (even the monologue) are connected in some strange way.',
+
+    episodes: 31,
+
+    cast: [
+      { actor: 'David Cross', character: 'Himself - Host', episodes: 30 },
+      { actor: 'Bob Odenkirk', character: 'Himself - Host', episodes: 30 },
+      { actor: 'John Ennis', character: 'Various', episodes: 30 },
+      { actor: 'Jay Johnston', character: 'Various', episodes: 28 },
+      { actor: 'Paul F. Tompkins', character: 'Various', episodes: 27 },
+      { actor: 'Jill Talley', character: 'Various', episodes: 26 },
+      { actor: 'Brett Paesel', character: 'Various', episodes: 21 },
+      { actor: 'Brian Posehn', character: 'Various', episodes: 21 },
+      { actor: 'Tom Kenny', character: 'Various', episodes: 20 },
+    ],
+  },
+
+  {
+    title: 'Seinfeld',
+
+    cardImage: 'https://thenypost.files.wordpress.com/2019/06/seinfeld-cast.jpg?quality=90&strip=all&w=618&h=410&crop=1',
+
+    description: 'Four single friends -- comic Jerry Seinfeld, bungling George Costanza, frustrated working gal Elaine Benes and eccentric neighbor Cosmo Kramer -- deal with the absurdities of everyday life in New York City.',
+
+    episodes: 173,
+ },
+    cast: [ },
+      { actor: 'Jerry Seinfeld', character: 'Jerry Seinfeld', episodes: 173 },
+      { actor: 'Michael Richards', character: 'Kramer', episodes: 173 },
+      { actor: 'Jason Alexander', character: 'George Costanza', episodes: 173 },
+      { actor: 'Julia Louis-Dreyfus', character: 'Elaine Benes', episodes: 172 },
+      { actor: 'Ruth Cohen', character: 'Ruthie Cohen', episodes: 101 },
+      { actor: 'Wayne Knight', character: 'Newman', episodes: 45 },
+      { actor: 'Norman Brenner', character: 'Man on Sidewalk', episodes: 29 },
+      { actor: 'Heidi Swedberg', character: 'Susan Ross', episodes: 28 },
+      { actor: 'Estelle Harris', character: 'Estelle Constanza', episodes: 27 },
+      { actor: 'Jerry Stiller', character: 'Frank Costanza', episodes: 26 },
+      { actor: 'John O\'Hurley', character: 'J. Peterman', episodes: 20 },
+    ],
+  },
+];
